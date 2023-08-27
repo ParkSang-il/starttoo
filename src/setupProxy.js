4669be208ebd557b2ea230796eb7a252175e4f67
박상일 <pju2251@nate.com>
2023-08-27 19:49:09 +0900

    프록시 url변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/feedlist', {
-            target: 'http://localhost:8080/api/v1',
+            target: 'http://api.service.kr:8080/api/v1',
             changeOrigin: true,
         }),
     );

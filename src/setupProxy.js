2bebb4efe16a3d48f281205f8cc15c037774c6a4
park_si <pju2251@nate.com>
2023-09-16 19:22:59 +0900

    API Proxy URL 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('/feedlist', {
+        createProxyMiddleware('/feed/feedlist', {
             target: 'http://api.starttoo.kr/api/v1',
             changeOrigin: true,
         }),

166e1924536a2e2d4d021ba35ec630a4852b2479
강승재 <kangsj45@gmail.com>
2023-10-18 20:15:25 +0900

    좋아요 push - 강승재

--- src/component/ListData.js
+++ src/component/ListData.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import '../assets/css/list.css'
+import axios from 'axios'
 
 export default function ListData(props) {
     var data =props.list; // 데이터 리스트
@@ -15,6 +16,25 @@ export default function ListData(props) {
 
     //팔로우
     const [follower, setFollwer] = useState([]);
+
+    //좋아요
+    const hitLike = async (idNum, _like, item) => {
+        console.log(idNum);
+        console.log(_like);
+        console.log(typeof idNum);
+        console.log(typeof _like);
+        try{
+            const res = await axios.post('/user/likeIt', {idx : idNum, like : _like});
+            console.log(res.data);
+            if(_like == 1){
+                setLike((like) => [...like, item]);
+            }else{
+                setLike(like.filter((button) => button !== item));
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }
 
     //신고
     const [block, setBlock] =useState();
@@ -35,6 +55,7 @@ export default function ListData(props) {
    
     //리스트 반복
     const dataList = data.map((item, idx) => {
+        console.log(item);
         return (
           <li key={item.idx}>
             <div className='tit-box'>
@@ -57,7 +78,8 @@ export default function ListData(props) {
             <div className='btn-box-wrap'>
                 <div className='btn-box txt-btn'>
                     <button className={'like' + (like.includes(item) ? ' on':'')} onClick={()=>{
-                        !like.includes(item) ? setLike((like) => [...like, item]) : setLike(like.filter((button) => button !== item));
+                        !like.includes(item) ? hitLike(item.idx, 1, item) : hitLike(item.idx, 2, item);
+                        /** List.js 의 함수들처럼 대략 35번줄 언저리에 item 안에 있는 게시물번호 이용해서 api 호출하는 함수 만들어보기! */
                        /* let likeCnt = [...like];
                         likeCnt[idx]++;
                         setLike(likeCnt);*/
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
     app.use(
         createProxyMiddleware([
             '/feed/feedlist',
-            '/feed/tattoo_thema'
+            '/feed/tattoo_thema',
+            '/user/likeIt'
         ], {
             target: 'http://api.starttoo.kr/api/v1',
             changeOrigin: true,
